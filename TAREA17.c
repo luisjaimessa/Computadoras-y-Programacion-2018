47420b00c2345ebf0612d936f1e7335e4ae30752
luisjaimessa <31594737+luisjaimessa@users.noreply.github.com>
2017-10-31 09:09:12 -0600

    Add files via upload

---
+++ TAREA17.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+
+
+int main(int argc, char *argv[])
+{
+    
+   int var1=5 ;
+   int var2=2 ;
+   int resultado=0 ; 
+    
+    // Igual a ?
+    resultado= var1 == var2 ;
+    printf("%d == %d= %d\n",var1, var2, resultado);
+  
+    //menor igual que?
+   resultado= var1 <= var2 ;
+   printf("%d <= %d=\t %d\n", var1, var2, resultado);
+   
+   //mayor que?
+   resultado= var1 > var2 ;
+   printf("%d > %d=\t %d\n", var1, var2, resultado);
+   
+   
+   //diferente de?
+   resultado= var1 != var2;
+   printf("%d != %d=%d\n", var1, var2, resultado);
+   
+   // uso de &&
+   resultado = (var1 > var2) && (var1 != var2);
+   printf("(%d > %d) && (%d != %d)=%d \n", var1, var2, var1, var2, resultado);
+    
+    
+  
+  system("PAUSE");	
+  return 0;
+}
